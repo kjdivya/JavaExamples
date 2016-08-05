024f26580bbdcc0ba4461d00a63183bb04baffa7
Divya J <kjdivya@gmail.com>
2016-08-05 16:46:33 +0530

    Updated with Bubble Sort code to accept Interface methods

--- JavaExamples/src/dataStructure/algorithms/BubbleSort.java
+++ JavaExamples/src/dataStructure/algorithms/BubbleSort.java
@@ -1,23 +1,38 @@
 package dataStructure.algorithms;
 
-public class BubbleSort 
+public class BubbleSort implements MySort
 {
+	static int[] array = null;
 	public static void main(String[] args) 
 	{
-		BubbleSort sort = new BubbleSort();
+		MySort sort = new BubbleSort();
 		
-		int[] array = new int[] {3,6,2,7,1,8,5};
+		array = new int[] {3,6,2,7,1,8,5};
+		
 		System.out.println("Before Sort");
 		sort.printArray(array);
 		
 		
-		sort.bubbleSort(array);
+		sort.sort(array);
 		
 		System.out.println("Before Sort");
 		sort.printArray(array);
 	}
 	
-	public void bubbleSort(int[] array)
+	public void printArray(int[] array)
+	{
+		int size = array.length;
+		
+		for(int i = 0 ; i < size; i++)
+		{
+			System.out.print(" "+ array[i]);
+		}
+		System.out.println();
+		System.out.println("****************");
+	}
+
+	@Override
+	public void sort(int[] array)
 	{
 		int temp = -1;
 		boolean isSorted = false;
@@ -41,18 +56,6 @@ public class BubbleSort
 			if(isSorted)
 				isSorted = false;
 		}
-	}
-	
-	public void printArray(int[] array)
-	{
-		int size = array.length;
-		
-		for(int i = 0 ; i < size; i++)
-		{
-			System.out.print(" "+ array[i]);
-		}
-		System.out.println();
-		System.out.println("****************");
 	}
 
 }
