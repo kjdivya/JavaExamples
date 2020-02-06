f179f2a00b7e299f2b7261e166511e00f553086e
Divya <kjdivya@gmail.com>
2020-02-06 11:22:14 +0530

    Simple sysout statement

--- JavaExamples/src/start/HelloWorld.java
+++ JavaExamples/src/start/HelloWorld.java
@@ -1,10 +1,11 @@
-package start;
+package src.start;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Divya - From Hello World");
+		System.out.println("Hello world !!!");
 	}
 
 }
